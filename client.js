e5eb529762d1b1f4cda9252073470e04d5f94b0e
marcelocp3 <marcelocp3@al.insper.edu.br>
2024-10-08 09:55:05 -0300

    Exercicios funcionando e sendo enviados

--- client.js
+++ client.js
@@ -19,12 +19,27 @@ function get_token() {
     });
 }
 
-// Função para submeter a resposta do exercício
-function submit_answer(slug, token, answer) {
+// Função para obter a lista de exercícios
+function get_exercises(token) {
+  return axios
+    .get("https://tecweb-js.insper-comp.com.br/exercicio", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`, // Incluímos o token no header Authorization
+      }
+    })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Erro ao obter a lista de exercícios:", error);
+    });
+}
+
+function submit_answer(token, slug , answer) {
   return axios
     .post(
       `https://tecweb-js.insper-comp.com.br/exercicio/${slug}`, // URL do exercício
-      { resposta: answer }, // Resposta no corpo da requisição
+      { resposta: answer }, // Corpo da requisição no formato JSON { resposta: valor }
       {
         headers: {
           "Content-Type": "application/json",
@@ -32,38 +47,55 @@ function submit_answer(slug, token, answer) {
           Authorization: `Bearer ${token}`, // Token de autenticação
         }
       }
-    )
-    .then((response) => {
-      console.log("Resposta submetida com sucesso:", response.data);
-    })
-    .catch((error) => {
-      console.error("Erro ao submeter a resposta:", error);
-    });
+    );
 }
 
-// Função principal
-async function main() {
+async function main(){
   try {
-    // Obtenção do token
+    // Obtemos o token
     let token = await get_token();
     console.log("Token:", token);
+    // Utilizamos o token para obter a lista de exercícios
 
-    // Exemplo do primeiro exercício
-    const slug = 'soma'; // Slug do exercício
-    const resultado = soma(6661, 1573); // Resolvendo o exercício
+    let exercises = await get_exercises(token);
+    console.log(exercises);
 
-    console.log(`O resultado de a + b é: ${resultado}`);
+    // const a = exercises['soma']['entrada']['a'];
+    // const b = exercises['soma']['entrada']['b'];
 
-    // Submetendo a resposta do exercício
-    await submit_answer(slug, token, resultado);
+    // console.log(a);
+    // console.log(b);
+
+    // const str = exercises['tamanho-string']['entrada']['string'];
+
+    const email = exercises['nome-do-usuario']['entrada']['email'];
+
+
+
+    const resultado = nome_usuario(email); // Resolvendo o exercício
+    
+    console.log(`O resultado é: ${resultado}`);
+    // Submetendo a resposta do exercício com o token e o resultado
+    await submit_answer(token,'nome-do-usuario',resultado);
+
   } catch (error) {
     console.error("Erro:", error);
-  }
+  };
+
 }
 
-// Função para somar valores (exercício 1)
-function soma(a, b) {
+function soma_valores(a, b) {
   return a + b;
 }
+
+function tamanho_string(str){
+  return str.length;
+}
+
+function nome_usuario(email){
+  return email.split('@')[0];
+}
+
+
 
 main();
