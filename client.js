2a6c23647ac1ca162a749f15c6c36b0a9f45d4b1
marcelocp3 <marcelocp3@al.insper.edu.br>
2024-10-08 19:04:12 -0300

    Limpando o código

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ function get_token() {
   return axios
     .post(
       "https://tecweb-js.insper-comp.com.br/token",
-      { username: "marcelocp3" }, // Substitua pelo seu nome de usuário Insper, se necessário
+      { username: "marcelocp3" }, 
       {
         headers: {
           "Content-Type": "application/json",
@@ -13,10 +13,7 @@ function get_token() {
         }
       }
     )
-    .then((response) => response.data.accessToken)
-    .catch((error) => {
-      console.error("Erro ao obter o token:", error);
-    });
+    .then((response) => response.data.accessToken);
 }
 
 // Função para obter a lista de exercícios
@@ -26,103 +23,93 @@ function get_exercises(token) {
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
-        Authorization: `Bearer ${token}`, // Incluímos o token no header Authorization
+        Authorization: `Bearer ${token}`, 
       }
     })
-    .then((response) => response.data)
-    .catch((error) => {
-      console.error("Erro ao obter a lista de exercícios:", error);
-    });
+    .then((response) => response.data);
 }
 
+// Função para enviar as respostas
 function submit_answer(token, slug , answer) {
   return axios
     .post(
-      `https://tecweb-js.insper-comp.com.br/exercicio/${slug}`, // URL do exercício
-      { resposta: answer }, // Corpo da requisição no formato JSON { resposta: valor }
+      `https://tecweb-js.insper-comp.com.br/exercicio/${slug}`, 
+      { resposta: answer }, 
       {
         headers: {
           "Content-Type": "application/json",
           Accept: "application/json",
-          Authorization: `Bearer ${token}`, // Obtenção do Token de autenticação
+          Authorization: `Bearer ${token}`,
         }
       }
     );
 }
 
 async function main(){
-  try {
-    // Obtemos o token
-    let token = await get_token();
-    console.log("Token:", token);
-    // Utilizamos o token para obter a lista de exercícios
-    let exercises = await get_exercises(token);
-    // Imprime a lista de exercícios
-    console.log(exercises);
+  // Obtendo e mostrando token
+  let token = await get_token();
+  console.log("Token:", token);
 
-    // Valores passados para o Ex1
-    const a = exercises['soma']['entrada']['a'];
-    const b = exercises['soma']['entrada']['b'];
-    const resultado_1 = soma(a, b); // Resolvendo o exercício
-    await submit_answer(token,"soma",resultado_1);
+  // Obtendo e mostrando lista de exercícios
+  let exercises = await get_exercises(token);
+  console.log(exercises);
 
-    // Valor passados para o Ex2
-    const str = exercises['tamanho-string']['entrada']['string'];
-    const resultado_2 = tamanho_string(str); // Resolvendo o exercício
-    await submit_answer(token,"tamanho-string",resultado_2);
+  //Ex1
+  const a = exercises['soma']['entrada']['a'];
+  const b = exercises['soma']['entrada']['b'];
+  const resultado_1 = soma(a, b); 
+  await submit_answer(token,"soma",resultado_1);
 
-    // Valores passados para o Ex3
-    const email = exercises['nome-do-usuario']['entrada']['email'];
-    const resultado_3 = nome_usuario(email); // Resolvendo o exercício
-    await submit_answer(token,"nome-do-usuario",resultado_3);
+  //Ex2
+  const str = exercises['tamanho-string']['entrada']['string'];
+  const resultado_2 = tamanho_string(str); 
+  await submit_answer(token,"tamanho-string",resultado_2);
 
-    // Valores passados para o Ex4
-    const v = exercises['jaca-wars']['entrada']['v'];
-    const theta = exercises['jaca-wars']['entrada']['theta'];
-    const resultado_4 = jaca_wars(v,theta); // Resolvendo o exercício
-    console.log(v);
-    console.log(theta);
-    console.log(resultado_4);
-    await submit_answer(token,"jaca-wars",resultado_4);
+  //Ex3
+  const email = exercises['nome-do-usuario']['entrada']['email'];
+  const resultado_3 = nome_usuario(email); 
+  await submit_answer(token,"nome-do-usuario",resultado_3);
 
-    // Valor passados para o Ex5
-    const ano = exercises['ano-bissexto']['entrada']['ano'];
-    const resultado_5 = bissexto(ano); // Resolvendo o exercício
-    await submit_answer(token,"ano-bissexto",resultado_5);
+  //Ex4
+  const v = exercises['jaca-wars']['entrada']['v'];
+  const theta = exercises['jaca-wars']['entrada']['theta'];
+  const resultado_4 = jaca_wars(v,theta); 
+  await submit_answer(token,"jaca-wars",resultado_4);
 
-    // Valores passados para o Ex 6
-    const raio = exercises['volume-da-pizza']['entrada']['z'];
-    const altura = exercises['volume-da-pizza']['entrada']['a'];
-    const resultado_6 = volume(raio,altura); // Resolvendo o exercício
-    await submit_answer(token,"volume-da-pizza",resultado_6);
+  //Ex5
+  const ano = exercises['ano-bissexto']['entrada']['ano'];
+  const resultado_5 = bissexto(ano); 
+  await submit_answer(token,"ano-bissexto",resultado_5);
+
+  //Ex 6
+  const raio = exercises['volume-da-pizza']['entrada']['z'];
+  const altura = exercises['volume-da-pizza']['entrada']['a'];
+  const resultado_6 = volume(raio,altura); 
+  await submit_answer(token,"volume-da-pizza",resultado_6);
     
-    // Valores passados para o Ex 7
-    const s0 = exercises['mru']['entrada']['s0'];
-    const vel = exercises['mru']['entrada']['v'];
-    const t = exercises['mru']['entrada']['t'];
-    const resultado_7 = mru(s0,vel,t); // Resolvendo o exercício  
-    await submit_answer(token,"mru",resultado_7);
+  //Ex 7
+  const s0 = exercises['mru']['entrada']['s0'];
+  const vel = exercises['mru']['entrada']['v'];
+  const t = exercises['mru']['entrada']['t'];
+  const resultado_7 = mru(s0,vel,t);   
+  await submit_answer(token,"mru",resultado_7);
 
-    // Valores passados para o Ex 8
-    const stri = exercises['inverte-string']['entrada']['string'];
-    const resultado_8 = inverte(stri); // Resolvendo o exercício
-    await submit_answer(token,"inverte-string",resultado_8);
+  //Ex 8
+  const stri = exercises['inverte-string']['entrada']['string'];
+  const resultado_8 = inverte(stri); 
+  await submit_answer(token,"inverte-string",resultado_8);
 
-    // Valores passados para o Ex 9
-    const objeto = exercises['soma-valores']['entrada']['objeto'];
-    const resultado_9 = soma_valores(objeto); // Resolvendo o exercício
-    submit_answer(token,"soma-valores",resultado_9);
+  //Ex 9
+  const objeto = exercises['soma-valores']['entrada']['objeto'];
+  const resultado_9 = soma_valores(objeto); 
+  submit_answer(token,"soma-valores",resultado_9);
 
-    // Valores passados para o Ex 10
-    const n = exercises['n-esimo-primo']['entrada']['n'];
-    const resultado_10 = n_primo(n); // Resolvendo o exercício
-    await submit_answer(token,"n-esimo-primo",resultado_10);
+  //Ex 10
+  const n = exercises['n-esimo-primo']['entrada']['n'];
+  const resultado_10 = n_primo(n); 
+  await submit_answer(token,"n-esimo-primo",resultado_10);
+  }
 
-  } catch (error) {
-    console.error("Erro:", error);
-  };
-
-}
 
 function soma(a, b) {
   return a + b;
